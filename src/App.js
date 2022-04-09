128dbcdd2e7d859dc1d628d6668076e99a5a11a7
anuj-kumary <anujf0510@gmail.com>
2022-04-09 15:58:01 +0530

    Feat : Landing Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { LandingPage } from './pages/Landing-Page/LandingPage';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Eagle Tv</h1>
+      <LandingPage />
     </div>
   );
 }
