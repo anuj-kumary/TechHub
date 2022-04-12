23cc72a154fffc1e96e4cbf523717c88e0322ebb
anuj-kumary <anujf0510@gmail.com>
2022-04-12 21:00:27 +0530

    name change

--- src/pages/Landing-Page/LandingPage.jsx
+++ src/pages/Landing-Page/LandingPage.jsx
@@ -5,10 +5,10 @@ export const LandingPage = () => {
   return (
     <>
       <div className='hero'>
-        <h1 className='hero__logo'>EagleTV</h1>
+        <h1 className='hero__logo'>TechHub</h1>
         <img src='./img/bg.jpg' alt='background' />
         <div className='hero__content'>
-          <h1 className='hero__heading'>Welcome to Eagle TV</h1>
+          <h1 className='hero__heading'>Welcome to TechHub</h1>
           <h2 className='sub__heading'>Watch anywhere anytime.</h2>
           <div className='hero__btn'>
             <a className='btn btn__warn' href='/'>
