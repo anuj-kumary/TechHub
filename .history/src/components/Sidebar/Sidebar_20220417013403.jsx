547c8a679ecea611386bd634d0433e3a875c5e46
anuj-kumary <anujf0510@gmail.com>
2022-04-17 22:56:16 +0530

    fix sidebar text color

---
+++ .history/src/components/Sidebar/Sidebar_20220417013403.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Sidebar.css';
+
+export const Sidebar = () => {
+  return (
+    <>
+      <aside className='sidebar'>
+        <ul className='sidebar__menu'>
+          <NavLink
+            to='/video'
+            activeClassName='active'
+            className='sidebar__link text'
+          >
+            <i className='fas fa-home'></i>Explore
+          </NavLink>
+          <NavLink
+            to='/playlist'
+            activeClassName='active'
+            className='sidebar__link text'
+          >
+            <i className='fas fa-play-circle'></i>Playlist
+          </NavLink>
+          <NavLink
+            to='/like'
+            activeClassName='active'
+            className='sidebar__link text'
+          >
+            <i className='fas fa-heart'></i>Liked
+          </NavLink>
+          <NavLink
+            to='/watchlater'
+            activeClassName='active'
+            className='sidebar__link text'
+          >
+            <i className='fas fa-clock'></i>Watch Later
+          </NavLink>
+          <NavLink
+            to='/history'
+            activeClassName='active'
+            className='sidebar__link text'
+          >
+            <i className='fas fa-history'></i> History
+          </NavLink>
+        </ul>
+      </aside>
+    </>
+  );
+};
